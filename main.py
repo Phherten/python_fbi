2b783822aa7227b7f90cd5af5a487c53294db9e0
Phherten <98804730+Phherten@users.noreply.github.com>
2022-09-07 00:43:22 +0200

    Funciones para meter en la carcel, funcion para al salir de la carcel retomar el antigüo jefe

--- main.py
+++ main.py
@@ -1,24 +1,68 @@
 import requests
-from models import MafiaMember, Jail
+from models import MafiaMember, Jail, Mafia
+from functions import print_members_free
 
 
 def main():
     list_of_members = []
+    list_of_jailed = []
 
-    # Paso 1
+    # Paso 1 Importartodo el grupo de mafiosos e imprimirlos
+
+    print('Estos son los miembros de la mafia:')
     response = requests.get('https://run.mocky.io/v3/6d754a01-9529-47fe-a6ee-8037836b8333')
     for member in response.json().get('members', []):
         list_of_members.append(MafiaMember(**member))
+        print_members_free(member)
+        # print_members_free(member)
 
-    # Paso 2
+    # Paso 2 encarcelar a Jhon
     jail = Jail()
     for member in list_of_members:
         if member.name == 'Jhon':
             jail.got_to_jail(member)
+            list_of_members.remove(member)
+            list_of_jailed.append(member)
+            print(f'Hemos encarcelado a {member.name}')
+            print("||||||||||||||||||||||||||||||||||||||")
 
-    jail.get_out_jail('Jhon')
+    # Paso 3 imprimir a la gente encarcelada
+    print("Estos mafiosos estan encarcelados:")
+    for i in list_of_jailed:
+        print(f'*{i.name}')
 
-    print(list_of_members.("name"))
+    print("|||||||||||||||||||||||||||||||||||||||||||")
+
+    # print(f'esta gente esta en la carcel{" y ".join(jail.members)}')
+
+    # Paso 4 Reagrupar a los subordinados de Jhon
+    for member in list_of_members:
+        if member.boss == "Jhon":
+            MafiaMember.add_boss(member, name="Pablo")
+    for member in list_of_members:
+        print(MafiaMember.__str__(member))
+
+    # Paso 5 Soltar a Jhon
+    for member in list_of_jailed:
+        if member.name == 'Jhon':
+            jail.get_out_jail(member)
+            list_of_members.append(member)
+            list_of_jailed.remove(member)
+            print(f'Hemos soltado a {member.name}')
+            print("||||                    |||||")
+
+    for member in list_of_members:
+        if member.name == 'Jhon':
+            subordinates = MafiaMember.get_subordinates(member)
+    for member in list_of_members:
+        for i in subordinates:
+            if i == member.name:
+                MafiaMember.add_boss(member, name="Jhon")
+
+    # Paso 6 Volver al antiguo organigrama
+
+    for member in list_of_members:
+        print(MafiaMember.__str__(member))
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class Mafia:
         return None
 
     # Cambiar nombre de metodo
-    def get_next_boos(self, member):
+    def get_next_boss(self, member):
         # 1. Buscar los datos del subordinado
         # 2. Comparar los subordinados
         subordinates = []
@@ -40,23 +40,27 @@ class MafiaMember:
         self.boss = boss
 
     def __str__(self):
-        return f'{self.name} - {self.seniority} - {self.subordinates} - {self.boss}'
+        return f'{self.name} tiene nivel {self.seniority} de antigüedad,' \
+               f'{" y ".join(self.subordinates) + " son sus subordinados" if self.subordinates else " "}' \
+               f'{"y su jefe actual es " + self.boss if self.boss else " y es el jefe"}'
+
+    def get_boss(self):
+        return self.boss
+
+    def add_boss(self, name):
+        self.boss = name
+        return self.boss
 
     def get_subordinates(self):
-        return self.subordinates
-
-    def add_subordinate(self, name):
-        self.subordinates.append(name)
-        return self.subordinates
-
-    def delete_subordinate(self, name):
-        self.subordinates.remove(name)
         return self.subordinates
 
 
 class Jail:
     def __init__(self, members=[]):
         self.members = members
+
+    def get_members_jail(self):
+        return self.members
 
     def got_to_jail(self, member):
         self.members.append(member)
@@ -67,18 +71,3 @@ class Jail:
                 self.members.pop(index)
 
         return self.members
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
