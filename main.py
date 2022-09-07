8fe5fd6cc5df765112aece11ef979c98714a6c67
Phherten <98804730+Phherten@users.noreply.github.com>
2022-09-07 12:38:47 +0200

    Funcion de reorganizar jefes terminada

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from models import MafiaMember, Jail, Mafia
+from models import MafiaMember, Jail
 from functions import print_members_free
 
 
@@ -14,17 +14,34 @@ def main():
     for member in response.json().get('members', []):
         list_of_members.append(MafiaMember(**member))
         print_members_free(member)
-        # print_members_free(member)
-
-    # Paso 2 encarcelar a Jhon
+    print("///////////////////////////////////////")
+    print("Escribe el nombre del detenido:")
+    nombre_detenido = input()
+    # Paso 2 encarcelar a Un miembro
     jail = Jail()
+    next_boss = None
+    subordinates = []
+    subordinate = None
+    boss = None
     for member in list_of_members:
-        if member.name == 'Jhon':
+        if member.name == nombre_detenido:
             jail.got_to_jail(member)
             list_of_members.remove(member)
             list_of_jailed.append(member)
+            boss = MafiaMember.get_boss(member)
             print(f'Hemos encarcelado a {member.name}')
             print("||||||||||||||||||||||||||||||||||||||")
+            for i in list_of_members:
+                if MafiaMember.get_equal_member(i, member):
+                    next_boss = MafiaMember.get_equal_member(i, member)
+                else:
+                    if MafiaMember.get_next_boss(i, member):
+                        subordinates.append(MafiaMember.get_next_boss(i, member))
+                        subordinates.sort(key=lambda x: x.seniority, reverse=True)
+                        subordinate = subordinates[0]
+            print(next_boss)
+            print(subordinate)
+            print(boss)
 
     # Paso 3 imprimir a la gente encarcelada
     print("Estos mafiosos estan encarcelados:")
@@ -33,18 +50,24 @@ def main():
 
     print("|||||||||||||||||||||||||||||||||||||||||||")
 
-    # print(f'esta gente esta en la carcel{" y ".join(jail.members)}')
+
 
     # Paso 4 Reagrupar a los subordinados de Jhon
     for member in list_of_members:
-        if member.boss == "Jhon":
-            MafiaMember.add_boss(member, name="Pablo")
+        if member.boss == nombre_detenido:
+            if next_boss:
+                MafiaMember.add_boss(member,next_boss.name)
+                MafiaMember.add_subordinates(member,next_boss)
+            elif member.name == subordinate.name:
+                MafiaMember.add_boss(member, boss)
+            else:
+                MafiaMember.add_boss(member, subordinate.name)
     for member in list_of_members:
         print(MafiaMember.__str__(member))
 
     # Paso 5 Soltar a Jhon
     for member in list_of_jailed:
-        if member.name == 'Jhon':
+        if member.name == nombre_detenido:
             jail.get_out_jail(member)
             list_of_members.append(member)
             list_of_jailed.remove(member)
@@ -52,12 +75,12 @@ def main():
             print("||||                    |||||")
 
     for member in list_of_members:
-        if member.name == 'Jhon':
+        if member.name == nombre_detenido:
             subordinates = MafiaMember.get_subordinates(member)
     for member in list_of_members:
         for i in subordinates:
             if i == member.name:
-                MafiaMember.add_boss(member, name="Jhon")
+                MafiaMember.add_boss(member, name=nombre_detenido)
 
     # Paso 6 Volver al antiguo organigrama
 
--- models.py
+++ models.py
@@ -1,37 +1,3 @@
-class Mafia:
-    def __init__(self, members=[]):
-        self.members = members
-
-    def get_equal_member(self, member):
-        # Cambiar nombre de variable element
-        for element in self.members:
-            if element.seniority == member.seniority:
-                return element
-        return None
-
-    # Cambiar nombre de metodo
-    def get_next_boss(self, member):
-        # 1. Buscar los datos del subordinado
-        # 2. Comparar los subordinados
-        subordinates = []
-        for element in self.members:
-            for sub in member.subordinates:
-                if sub == element.name:
-                    subordinates.append(element)
-
-        subordinates.sort(key=lambda x: x.seniority, reverse=True)
-        return subordinates[0]
-
-    def add_new_boss(self, member):
-        boss = self.get_equal_member(member)
-        if boss:
-            boss = self.get_next_boos(member)
-
-        for element in self.members:
-            if element.name in member.subordinates:
-                element.boss = boss.name
-
-
 class MafiaMember:
     def __init__(self, name, seniority=0, subordinates=[], boss=""):
         self.name = name
@@ -51,8 +17,36 @@ class MafiaMember:
         self.boss = name
         return self.boss
 
+    def add_subordinates(self, name):
+        self.subordinates.append(name)
+
     def get_subordinates(self):
         return self.subordinates
+
+    def get_equal_member(self, member):
+        # Cambiar nombre de variable element
+        if self.seniority == member.seniority:
+            return self
+        return None
+
+    # Cambiar nombre de metodo
+    def get_next_boss(self, member):
+        # 1. Buscar los datos del subordinado
+        # 2. Comparar los subordinados
+        if self.boss == member.name:
+            return self
+
+        # subordinates.sort(key=lambda x: x.seniority, reverse=True)
+        # return subordinates[0]
+
+    def add_new_boss(self, member):
+        boss = self.get_equal_member(member)
+        if boss:
+            boss = self.get_next_boss(member)
+
+        for element in self:
+            if element.name in member.subordinates:
+                element.boss = boss.name
 
 
 class Jail:
